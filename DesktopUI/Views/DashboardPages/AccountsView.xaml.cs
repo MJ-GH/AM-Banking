8c272dd0b224e4c86784a2c08c8d3427ba0be3c0
Martin Jensen <mart924p@elev.tec.dk>
2019-11-19 10:42:42 +0100

    AccountsView shows content
    AccountsView now properly loads the content of AccountsOverview.

--- DesktopUI/Views/DashboardPages/AccountsView.xaml.cs
+++ DesktopUI/Views/DashboardPages/AccountsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using DesktopUI.Views.DashboardPages.AccountsPages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,11 @@ namespace DesktopUI.Views.DashboardPages
         public AccountsView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            ActiveItem.Content = new AccountsOverviewView();
         }
     }
 }
