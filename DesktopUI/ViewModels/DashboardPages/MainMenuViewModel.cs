892199b342ba6ef4ff6b1db600d43c973d24ae90
Martin Jensen <mart924p@elev.tec.dk>
2019-11-27 13:47:08 +0100

    Custom greetings message
    Depending on the time of day, you are greeted with a corresponding message.

--- DesktopUI/ViewModels/DashboardPages/MainMenuViewModel.cs
+++ DesktopUI/ViewModels/DashboardPages/MainMenuViewModel.cs
@@ -10,9 +10,11 @@ namespace DesktopUI.ViewModels.DashboardPages
 {
     public class MainMenuViewModel : Screen
     {
+        private string msg;
         private string _firstName;
         private string _lastName;
         private IEventAggregator _events;
+        private int _timeOftheDay;
 
         public string FirstName
         {
@@ -36,14 +38,38 @@ namespace DesktopUI.ViewModels.DashboardPages
         }
         public string FullName
         {
-            get { return $"{ FirstName } { LastName }"; }
+            get
+            {
+                if(_timeOftheDay >= 6 && _timeOftheDay < 9) // 06:00 - 08:59
+                {
+                    msg = $"Godmorgen { FirstName }";
+                }
+                else if (_timeOftheDay >= 9 && _timeOftheDay < 12) // 09:00 - 11:59
+                {
+                    msg = $"God formiddag { FirstName }";
+                }
+                else if (_timeOftheDay >= 12 && _timeOftheDay < 18) // 12:00 - 17:59
+                {
+                    msg = $"God eftermiddag { FirstName }";
+                }
+                else if (_timeOftheDay >= 18 && _timeOftheDay < 24) // 18:00 - 23:59
+                {
+                    msg = $"Godaften { FirstName }";
+                }
+                else if (_timeOftheDay >= 0 && _timeOftheDay < 6) // 00:00 - 05:59
+                {
+                    msg = $"Natuglen, { FirstName }";
+                }
+
+                return msg;
+            }
         }
 
         public MainMenuViewModel(IEventAggregator events)
         {
             _firstName = DashboardViewModel.u.FirstName;
             _lastName = DashboardViewModel.u.LastName;
-
+            _timeOftheDay = DateTime.Now.Hour;
             _events = events;
         }
 
